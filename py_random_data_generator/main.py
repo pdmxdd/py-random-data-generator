9b1d41cb5524ca4ad9cc1fbdf2c18b0a0c333d91
Paul Matthews <paul@paulmatthews.dev>
2022-04-30 14:39:46 -0500

    additional manual testin g of writer

--- py_random_data_generator/main.py
+++ py_random_data_generator/main.py
@@ -2,7 +2,10 @@ from generate_random import get_users_list, get_sensitive_users_list, get_transa
 
 from random_writer import write_dict_list_to_csv
 
-write_dict_list_to_csv('test.csv', get_users_list(11))
+write_dict_list_to_csv('test-user.csv', get_users_list(11))
+write_dict_list_to_csv('test-sensitive.csv', get_sensitive_users_list(11))
+write_dict_list_to_csv('test-transactions.csv', get_transactions_list(11))
+write_dict_list_to_csv('test-logs.csv', get_logs_list(11))
 
 # print(user_data_dict())
 
